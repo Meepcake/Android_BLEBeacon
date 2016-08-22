a0a7c2c7a622e6d9994da861bb578744873b899f
nikunj113 <nikunj1131@gmail.com>
2016-08-22 10:51:29 -0400

    Created new UI
    Added new graphics

--- app/src/main/java/com/meepcake/blebeacon/MainActivity.java
+++ app/src/main/java/com/meepcake/blebeacon/MainActivity.java
@@ -1,8 +1,12 @@
 package com.meepcake.blebeacon;
 
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,18 +16,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "BLEBeacon";
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * The {@link PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * {@link FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -31,14 +41,19 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -50,8 +65,51 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.meepcake.blebeacon/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.meepcake.blebeacon/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 
     /**
      * A placeholder fragment containing a simple view.
@@ -89,25 +147,53 @@ public class MainActivity extends AppCompatActivity {
 
 
                 // Scan
-                v = inflater.inflate(R.layout.fragment_main, container, false);
+                v = inflater.inflate(R.layout.fragment_scan_bt, container, false);
             } else if (pageNum == 2) {
+                final ImageButton unlockButton, lockButton, remoteStart, remoteStop, lightsAndHorns;
+                final ImageView semiTransparentBackground, spinner;
                 // Transmit
-                v = inflater.inflate(R.layout.fragment_main, container, false);
+                v = inflater.inflate(R.layout.fragment_transmit_beacon, container, false);
+                // Map All Buttons
+                unlockButton = (ImageButton) v.findViewById(R.id.btnUnlock);
+                lockButton = (ImageButton) v.findViewById(R.id.btnLock);
+                remoteStart = (ImageButton) v.findViewById(R.id.btnRemoteStart);
+                remoteStop = (ImageButton) v.findViewById(R.id.btnRemoteStop);
+                lightsAndHorns = (ImageButton) v.findViewById(R.id.btnHornAndLights);
+                semiTransparentBackground = (ImageView) v.findViewById(R.id.semi_transparent_cover);
+                spinner = (ImageView) v.findViewById(R.id.spinner);
 
+                unlockButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // click button code
+
+                        semiTransparentBackground.setVisibility(View.VISIBLE);
+                        spinner.setVisibility(View.VISIBLE);
+
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                semiTransparentBackground.setVisibility(View.INVISIBLE);
+                                spinner.setVisibility(View.INVISIBLE);
+                            }
+                        }, 5000);
+                    }
+                });
 
             } else if (pageNum == 3) {
                 // Info
-                v = inflater.inflate(R.layout.fragment_main, container, false);
+                v = inflater.inflate(R.layout.fragment_scan_beacon, container, false);
 
 
             } else if (pageNum == 4) {
                 // Info
-                v = inflater.inflate(R.layout.fragment_main, container, false);
+                v = inflater.inflate(R.layout.fragment_test, container, false);
 
 
             } else {
                 // How the hell would we get here? ._.
-                v = inflater.inflate(R.layout.fragment_main, container, false);
+                v = inflater.inflate(R.layout.fragment_scan_bt, container, false);
             }
 
             return v;
@@ -153,4 +239,5 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
+
 }
